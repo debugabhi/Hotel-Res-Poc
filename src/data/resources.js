72546bb7ff1c9995e87562db41afbf7388c293f6
Diwakar Kumar <diwkumar1@publicisgroupe.net>
2019-07-25 15:10:40 +0530

    Json files added

---
+++ src/data/resources.js
@@ -0,0 +1,22 @@
+export default [
+    {
+         id: 'r0',
+         name: 'Confirmation no1',
+    },
+    {
+         id: 'r1',
+         name: 'Confirmation no2'
+    },
+    {
+         id: 'r2',
+         name: 'Confirmation no3',
+    },
+    {
+         id: 'r3',
+         name: 'Confirmation no4',
+    },
+    {
+         id: 'r4',
+         name: 'Confirmation no5',
+    },
+];
